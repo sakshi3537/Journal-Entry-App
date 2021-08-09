4e2b42941c0efb49b93845a01537c6053d7df72a
sakshi3537 <sakshimandloi2001@gmail.com>
2021-08-09 22:07:54 +0530

    Final Version 1.2

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ const secret='secret';
 
 const auth = async(req,res,next) => {
     try{
-        const token = req.headers.authorization.split(" ")[1];
+        const token = req?.headers?.authorization.split(" ")[1];
         const decodedData = jwt.verify(token, secret);
         req.userId = decodedData?.id;
     }
