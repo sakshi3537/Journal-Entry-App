e5f9a0584a14a1b15c1c0fce5bfbdee58302e8aa
Jagruthi2001 <jagruthipatibandla@gmail.com>
2021-08-09 23:24:28 +0530

    Deployed version

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ import mongoose from 'mongoose'
 import Cards from './routes/cards.js'
 import Auth from './routes/auth.js'
 import User from './routes/users.js'
+import dotenv from 'dotenv';
+
 const app=express();
+dotenv.config();
+
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -15,11 +19,15 @@ app.use('/cards',Cards);
 app.use('/auth',Auth);
 app.use('/',User);
 
-const CONNECTION_URL="mongodb+srv://Sakshi:sakshi@cluster0.aq31q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app.get('/', (req,res) => {
+  res.send("Welcome to Journal Entry")
+
+})
+
 
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
